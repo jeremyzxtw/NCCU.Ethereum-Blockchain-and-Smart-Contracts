d1bfcb30255ced289a6d17fdd045a44fec564cdb
jeremyzxtw <jeremyzxtw@gmail.com>
2022-04-25 09:28:20 +0800

    first commit

---
+++ Lottery2.sol
@@ -0,0 +1,103 @@
+pragma solidity ^0.5.0;
+
+contract Lottery{
+    uint public phase=1;
+    uint public player_num_temp=0; 
+    uint public winner_num_temp=0;
+    struct lottery_num{
+        uint one;
+        uint two;
+        uint three;
+        uint four;
+        uint five;
+        uint six;
+        
+    }
+    
+    mapping(uint => lottery_num) public lottery;
+    
+    mapping(address => mapping(uint=>mapping(uint=> lottery_num))) public buy;
+    mapping(uint => uint) public players_count;//該期客戶總共數量
+    mapping(uint => uint) public winners_count;//該期贏家總共數量
+    mapping(address => mapping(uint =>uint)) public user_phase_count;//客戶第n期買幾次
+    mapping(uint => address payable[25]) public users_address_everyround;//每一期參與的帳號;
+    mapping(uint=> address payable[25]) public winner_address_everyround;
+    
+    
+    function drawing(uint a,uint b,uint c,uint d,uint e,uint f) public payable {
+        lottery[phase].one=a;
+        lottery[phase].two=b;
+        lottery[phase].three=c;
+        lottery[phase].four=d;
+        lottery[phase].five=e;
+        lottery[phase].six=f;
+        findwinner();
+        require(winners_count[phase]>0,"no winner this round!");
+        findwinner2();
+        phase=phase+1;
+        player_num_temp=0;
+        winner_num_temp=0;
+    }
+   
+    
+    function findwinner() public 
+    {
+        winners_count[phase]==0;
+        for(uint i=0 ;i< player_num_temp;i++)
+        {
+            for(uint k=0;k<user_phase_count[users_address_everyround[phase][i]][phase];k++)
+            {
+                if(buy[users_address_everyround[phase][i]][phase][k].one==lottery[phase].one && buy[users_address_everyround[phase][i]][phase][k].two==lottery[phase].two && buy[users_address_everyround[phase][i]][phase][k].three==lottery[phase].three && buy[users_address_everyround[phase][i]][phase][k].four==lottery[phase].four && buy[users_address_everyround[phase][i]][phase][k].five==lottery[phase].five  && buy[users_address_everyround[phase][i]][phase][k].six==lottery[phase].six)
+                {
+                winner_address_everyround[phase][winners_count[phase]]=users_address_everyround[phase][i];
+                winners_count[phase]+=1;
+                winner_num_temp+=1;
+                }
+                
+            }
+            
+        }
+        
+    }
+    
+    
+    function win_lottery(address payable dest,uint winning_prize) public payable
+    {
+        dest.transfer(winning_prize);
+    }
+    
+    
+    function findwinner2() public payable
+    {
+        uint prize = uint(address(this).balance)/winner_num_temp;
+        for(uint i=0; i< winners_count[phase];i++)
+        {
+            win_lottery(winner_address_everyround[phase][i],prize);
+        }
+    }
+    
+    function buying(uint a,uint b,uint c,uint d,uint e,uint f) public payable {
+        require(msg.value>=100000000000000000,"No money mo lottery, poor ass.");
+        require(a<=5 && a>=1,"please selcet the number between 1-5.");
+        buy[msg.sender][phase][user_phase_count[msg.sender][phase]].one=a;
+        buy[msg.sender][phase][user_phase_count[msg.sender][phase]].two=b;
+        buy[msg.sender][phase][user_phase_count[msg.sender][phase]].three=c;
+        buy[msg.sender][phase][user_phase_count[msg.sender][phase]].four=d;
+        buy[msg.sender][phase][user_phase_count[msg.sender][phase]].five=e;
+        buy[msg.sender][phase][user_phase_count[msg.sender][phase]].six=f;
+        if(user_phase_count[msg.sender][phase]==0)
+        {
+            users_address_everyround[phase][player_num_temp]=msg.sender;
+            player_num_temp+=1;
+        }
+        user_phase_count[msg.sender][phase]=user_phase_count[msg.sender][phase]+1;
+        players_count[phase]=players_count[phase]+1;
+
+    }
+    function () external payable 
+    {
+        revert();
+    }
+    
+
+}
---
+++ Tutorial/app.js
@@ -0,0 +1,67 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var register = require('./routes/register');
+
+// DataBase
+var mysql = require("mysql");
+
+var pool = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "123456",
+    database: "tutorial"
+});
+
+pool.getConnection(function (err) {
+  if (err) {
+      console.log('connecting error');
+      return;
+  }
+  console.log('connecting success');
+});
+
+var app = express();
+
+//Database
+app.use(function (req, res, next) {
+  req.connection = pool;
+  next();
+});
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/register', register);
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
